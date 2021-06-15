fef8231534ed77447cba33b9b1a51ed3a74df4dc
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-15 18:48:41 +0300

    adds delete method

--- src/com/company/trees/Main.java
+++ src/com/company/trees/Main.java
@@ -14,12 +14,19 @@ public class Main {
         intTree.insert(32);
 
         intTree.traverseInOrder();
+        System.out.println();
 
-        System.out.println(intTree.get(27));
-        System.out.println(intTree.get(15));
-        System.out.println(intTree.get(888));
+//        System.out.println(intTree.get(27));
+//        System.out.println(intTree.get(15));
+//        System.out.println(intTree.get(888));
+//
+//        System.out.println(intTree.min());
+//        System.out.println(intTree.max());
 
-        System.out.println(intTree.min());
-        System.out.println(intTree.max());
+        intTree.delete(15);
+//        intTree.delete(27);
+
+        intTree.traverseInOrder();
+        System.out.println();
     }
 }
